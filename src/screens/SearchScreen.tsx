50692281d7cbab636ecf263b77624542e9f1d628
AkibaRika <lxclxc89816@gmail.com>
2022-06-04 12:52:32 +0800

    📦️ updating react navigation v6 and adding search screen

---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const SearchScreen = () => {
+  return <View>
+    <Text>Search Screen</Text>
+  </View>
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
